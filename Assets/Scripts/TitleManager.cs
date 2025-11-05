e558b7e1967bbbd0aad5bfcf2aff63b141d66bc4
ChipiChipi0918 <yangi0918001@gmail.com>
2025-11-05 22:45:29 +0900

    따이뜰

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public void Play()
+    {
+        //ㄱㄱ
+    }
+
+    public void Opition()
+    {
+        //설정 유아이 팝업
+    }
+
+    public void Credit()
+    {
+        //크레딧
+    }
+
+    public void Leave()
+    {
+        //나가기
+    }
+}
