1195cd413785e2bd8bff8916f7be8d42671ccf36
lake04 <wjdghksrla0409@gmail.com>
2025-11-25 00:46:33 +0900

    땅 정령 돌 소환 스킬 추가(스킬 2번째꺼)

--- Assets/Scripts/Spirit/GroundSpirit.cs
+++ Assets/Scripts/Spirit/GroundSpirit.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GroundSpirit : SpiritBase
 {
+    [SerializeField] private LayerMask checkLayer;       
+    [SerializeField] private Vector3 checkRange;      
+    [SerializeField] private float groundCheckRayLength = 0.5f;
+
+    [SerializeField] private GameObject stonSpawnPos;
+    [SerializeField] private GameObject stonPrefab;
+
+
     void Start()
     {
         
@@ -12,6 +20,16 @@ public class GroundSpirit : SpiritBase
     void Update()
     {
         
+    }
+
+    public override void Skill1()
+    {
+        if(CanPlaceRock(stonSpawnPos.transform.position))
+        {
+            GameObject ston = Instantiate(stonPrefab,stonSpawnPos.transform.position,Quaternion.identity);
+        }
+
+
     }
 
     public override void Skill2()
@@ -28,4 +46,55 @@ public class GroundSpirit : SpiritBase
         {
         }
     }
+
+
+    public bool CanPlaceRock(Vector3 _pos)
+    {
+        Collider2D hit = Physics2D.OverlapBox(_pos, checkRange, 0, checkLayer);
+
+        if (hit != null)
+        {
+            if (hit.CompareTag("Wall"))
+            {
+                return false;
+            }
+        }
+ 
+        Vector2 rayOrigin = new Vector2(_pos.x, _pos.y - checkRange.y / 2f); 
+
+        RaycastHit2D groundHit = Physics2D.Raycast(
+            rayOrigin,
+            Vector2.down,
+            groundCheckRayLength, 
+            checkLayer 
+        );
+
+        if (groundHit.collider != null)
+        {
+            if (groundHit.collider.CompareTag("Ground"))
+            {
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (stonSpawnPos != null)
+        {
+            Vector3 spawnPos = stonSpawnPos.transform.position;
+            spawnPos.y += 1;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(spawnPos, checkRange);
+
+            Vector3 rayOrigin = new Vector3(spawnPos.x, spawnPos.y - checkRange.y / 2f);
+            Vector3 rayEnd = rayOrigin + Vector3.down * groundCheckRayLength;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(rayOrigin, rayEnd);
+        }
+    }
+
 }
