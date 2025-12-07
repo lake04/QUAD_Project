f7a50a2adfc946182b1e3cd0fbbbb97f966f451f
lake04 <wjdghksrla0409@gmail.com>
2025-12-07 21:59:10 +0900

    지역 넘어갈시 컷씬 나오게 추가

--- Assets/Scripts/Map/RegionTrigger.cs
+++ Assets/Scripts/Map/RegionTrigger.cs
@@ -1,11 +1,14 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RegionTrigger : MonoBehaviour
 {
     [SerializeField] private RegionText text;
-    [SerializeField] private Transform canva;
+    [SerializeField] private Image image;
+    [SerializeField] private Transform spawnPos;
 
     void Start()
     {
@@ -17,11 +20,31 @@ public class RegionTrigger : MonoBehaviour
         
     }
 
+    public IEnumerator InvokeImgCoroutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        Sequence sequence = DOTween.Sequence();
+        image.color = new Color(0, 0, 0, 0);
+
+        //gameObject.SetActive(true);
+        image.enabled = true;
+        image.gameObject.SetActive(true);
+        sequence.Append(image.DOFade(1.0f, 1.5f));
+        sequence.Append(image.DOFade(0f, 1.5f));
+
+
+        yield return new WaitForSeconds(1.5f);
+        GameManager.Instance.player.transform.position = spawnPos.position;
+
+        StartCoroutine(text.InvokeTextCoroutine());
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            StartCoroutine(text.InvokeTextCoroutine());
+            StartCoroutine(InvokeImgCoroutine());
         }
     }
 }
